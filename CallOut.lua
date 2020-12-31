3242ffc049815239f272884756d594442ef0e39f
Mike Davies <davies.michael.j@gmail.com>
2020-12-31 07:09:10 -0500

    add logic to reduce spam

--- CallOut.lua
+++ CallOut.lua
@@ -1,56 +1,42 @@
 local CreateFrame = CreateFrame
+local PlaySoundFile = PlaySoundFile
+
 local pairs = pairs
 local print = print
 local tinsert = table.insert
 
-local SpellcastStarts = {}
-local SpellcastSucceeds = {}
-
 CallOut = {}
 
-local function PlayCallOut(sound)
-  if CallOut.Settings.Enabled then
-    print(sound)
-  end
-end
 
 local function HandleEvent(self, event, ...)
-	CallOut.Events[event].Handle(self, event, ...)
+	CallOut.Events[event](self, event, ...)
 end
 
 local function HandleLogin(self, event, ...)
-	for event, eventInfo in pairs(CallOut.Events) do
-    if eventInfo.Units == nil then
-      CallOut.RegisteredEventUpdateFrame:RegisterEvent(event)
-    else
-      CallOut.RegisteredEventUpdateFrame:RegisterUnitEvent(event, eventInfo.Units[1], eventInfo.Units[2])
-    end
+	for event in pairs(CallOut.Events) do
+    CallOut.RegisteredEventUpdateFrame:RegisterEvent(event)
 	end
 end
 
-local function HandleUnitSpellcastStart(self, event, unitTarget, castGUID, spellID)
-  if CallOut.Settings.Targets[unitTarget] and CallOut.Settings.SpellIDs[spellID] then
-    PlayCallOut(CallOut.Sounds[spellID].Name)
-  end  
+local function ShouldPlayCallOut(event, unitTarget, spellID)
+  if not CallOut.Settings.Enabled then return false end
+  if not CallOut.Settings.Targets[unitTarget] then return false end
+  if not CallOut.Settings.SpellIDs[spellID].Enabled then return false end
+  if not CallOut.Settings.SpellIDs[spellID].InstantCast and event == "UNIT_SPELLCAST_SUCCEEDED" then return false end
+  return true
 end
 
-local function HandleUnitSpellcastChannelStart(self, event, unit, castGUID, spellID)
-  if CallOut.Settings.Targets[unitTarget] and CallOut.Settings.SpellIDs[spellID] then
-    PlayCallOut(CallOut.Sounds[spellID].Name)
-  end  
-end
-
-local function HandleUnitSpellcastSucceeded(self, event, unit, castGUID, spellID)
-  if CallOut.Settings.Targets[unitTarget] and CallOut.Settings.SpellIDs[spellID] then
-    PlayCallOut(CallOut.Sounds[spellID].Name)
-  end  
+local function HandleUnitSpellcast(self, event, unitTarget, castGUID, spellID)
+  if ShouldPlayCallOut(event, unitTarget, spellID) then
+    PlaySoundFile(CallOut.Sounds[spellID].File, CallOut.Settings.SoundChannel)
+  end
 end
 
 CallOut.Events = {
-  PLAYER_LOGIN = {Units = nil, Handle = HandleLogin},
-  UNIT_SPELLCAST_START = {Units = nil, Handle = HandleUnitSpellcastStart},  -- regular spell casts
-  UNIT_SPELLCAST_CHANNEL_START = {Units = nil, Handle = HandleUnitSpellcastChannelStart}, -- channeling spells
-  UNIT_SPELLCAST_SUCCEEDED = {Units = nil, Handle = HandleUnitSpellcastSucceeded} -- instant spell casts
+  PLAYER_LOGIN = HandleLogin,
+  UNIT_SPELLCAST_START = HandleUnitSpellcast,  -- regular spell casts
+  UNIT_SPELLCAST_CHANNEL_START = HandleUnitSpellcast, -- channeling spells
+  UNIT_SPELLCAST_SUCCEEDED = HandleUnitSpellcast -- instant spell casts
 }
 
 CallOut.RegisteredEventUpdateFrame = CreateFrame("Frame", nil, UIParent)
--- CallOutSettings.lua
+++ CallOutSettings.lua
@@ -1,13 +1,20 @@
 CallOut.Settings = {
   Enabled = true,
-  Targets = {target = true, 
-             arena1 = true, 
-             arena2 = true, 
-             arena3 = true, 
-             arena4 = true, 
-             arena5 = true},
-
-  SpellIDs = {[585]   = true, 
-              [47549] = true, 
-              [17]    = true}
+  
+  Targets = {
+    target = true, 
+    arena1 = true, 
+    arena2 = true, 
+    arena3 = true, 
+    arena4 = true, 
+    arena5 = true
+  },
+  
+  SpellIDs = {
+    [585]   = {Enabled = true, InstantCast = false},
+    [47549] = {Enabled = true, InstantCast = false}, 
+    [17]    = {Enabled = true, InstantCast = true}
+  },
+  
+  SoundChannel = "Master"
 }
